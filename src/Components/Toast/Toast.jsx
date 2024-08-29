8725ee35a4a06e1eb4bdd0185eb7351d7ad55d7c
Amina AbdelAzeez <aminaabdelazeez99@gmail.com>
2024-08-29 11:19:06 +0300

    first commit

--- src/Components/Toast/Toast.jsx
+++ src/Components/Toast/Toast.jsx
@@ -1,6 +1,17 @@
+import React, { useEffect } from "react";
 import styles from "./Toast.module.css";
 
 const Toast = ({ message, onClose }) => {
+  useEffect(() => {
+    if (!message) return;
+
+    const timer = setTimeout(() => {
+      onClose();
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, [message, onClose]);
+
   if (!message) return null;
 
   return (
