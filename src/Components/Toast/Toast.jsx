1b246d716289e78f0ca81e563239518a930ceb45
Amina AbdelAzeez <aminaabdelazeez99@gmail.com>
2024-08-29 12:27:28 +0300

    first commit

--- src/Components/Toast/Toast.jsx
+++ src/Components/Toast/Toast.jsx
@@ -3,25 +3,20 @@ import styles from "./Toast.module.css";
 
 const Toast = ({ message, onClose }) => {
   useEffect(() => {
-    if (!message) return;
+    if (message) {
+      const timer = setTimeout(() => {
+        onClose();
+      }, 2500);
 
-    const timer = setTimeout(() => {
-      onClose();
-    }, 2000);
-
-    return () => clearTimeout(timer);
+      return () => clearTimeout(timer);
+    }
   }, [message, onClose]);
 
-  if (!message) return null;
-
-  return (
+  return message ? (
     <div className={styles.toast}>
       <p>{message}</p>
-      <button onClick={onClose} className={styles.toastClose}>
-        X
-      </button>
     </div>
-  );
+  ) : null;
 };
 
 export default Toast;
